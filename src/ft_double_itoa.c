16db0326bfb1b5907f37658784cdc0934eb14d37
brokenFdreams <brokenFdreams@protonmail.com>
2019-07-02 17:40:19 +0300

    add new functions

--- src/ft_double_itoa.c
+++ src/ft_double_itoa.c
@@ -6,7 +6,7 @@
 /*   By: fsinged <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/01 15:50:28 by fsinged           #+#    #+#             */
-/*   Updated: 2019/07/02 15:45:36 by fsinged          ###   ########.fr       */
+/*   Updated: 2019/07/02 17:39:55 by fsinged          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,20 +93,99 @@ static char	*ft_double_except(char *binary, char **num, int exp)
 	return (NULL);
 }
 
-static char	*ft_double_revert(char *mantissa, char **num, int exponent)
-{
-	char	*integer;
-	size_t	i;
-	size_t	power;
+/*
+** Plus remainder
+*/
 
-	if (exponent >= 0)
+static void ft_double_plusrem(char **num, int size)
+{
+	char *fund;
+
+	if (size = 0)
 	{
-		if (!(integer = ft_strnew(652)))
+		if (!(fund = ft_strjoin("0", *num)))
 			ft_error();
-		i = 0;
-		while (i < exponent)
-			i++;
-		integer =
+		ft_strdel(num);
+		(*num) = fund;
+	}
+	if ((*num)[size] == 9)
+	{
+		(*num)[size] = '0';
+		ft_double_plusrem(*num, size - 1);
+	}
+	else if ((*num)[size] == '.')
+		ft_double_plusrem(*num, size - 1);
+	else
+		(*num)[size] += 1;	
+}
+
+/*
+** Addition in the string
+** Return its value
+*/
+
+static void	ft_double_addition(char **num, char *tmp, int size, int len)
+{
+	int a;
+	int b;
+
+	b = 0;
+	while (size >= 0 && len >= 0 && (*num)[size] != '.')
+	{
+		if (ft_isdigit((*num)[size]))
+			a = (*num)[size] - '0';
+		else
+			a = 0;
+		b += tmp[len--] - '0';
+		a = a + b;
+		b = a > 10 ? 1 : 0;
+		(*num)[size--] = a > 10 ? a % 10 + '0' : a + '0';
+	}
+	if (b)
+		ft_double_plusrem(num, size);
+}
+
+/*
+** Revert double to decemical in string
+** Return its value
+*/
+
+static void	ft_double_revert(char *mantissa, char **num, int exponent)
+{
+	char		*tmp;
+	char		*remainder;
+	uintmax_t	power;
+	size_t		i;
+	int			size;
+	int			a;
+	int			dot;
+	int			len;
+
+	size = 652;
+	if (!(num = ft_strnew(size)))
+		ft_error();
+	power = ft_exponentiation(exponent, 2);
+	i = 0;
+	while (i < exponent)
+	{
+		tmp = ft_uint_itoa(power * (mantissa[i] - '0'));
+		len = ft_strlen(tmp) - 1;
+		ft_double_addition(num, tmp, len, len);
+		i++;
+		power /= 2;
+		ft_strdel(&tmp);
+	}
+	i = ft_strlen(*num);
+	(*num)[i] = '.';
+	dot = i - 1;
+	power = 5;
+	while (i < size)
+	{
+		tmp = ft_uint_itoa(power * (mantissa[i] - '0'));
+		len = ft_strlen(tmp);
+		ft_double_addition(num, tmp, i, len);
+		power *= 5;
+		i++;
 	}
 }
 
