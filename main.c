5e085665a21f4d0c5e4697f73a9fec7559ab96f3
brokenFdreams <brokenFdreams@protonmail.com>
2019-07-12 17:11:35 +0300

    save

--- main.c
+++ main.c
@@ -6,24 +6,16 @@
 /*   By: fsinged <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/10 12:35:06 by fsinged           #+#    #+#             */
-/*   Updated: 2019/07/10 16:35:23 by fsinged          ###   ########.fr       */
+/*   Updated: 2019/07/12 17:03:39 by fsinged          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/ft_printf.h"
 
-char        *ft_double_power(int exponent, int base);
 
 int		main(void)
 {
-	char *str;
-//	char *tmp;
-
-	ft_double_exp(0, 2, &str);
-	printf("%s\n", str);
-//	ft_strdel(&str);
-//	tmp = ft_double_power(1026, 5);
-//	printf("\n%s\n", tmp);
-//	ft_strdel(&tmp);
+	int i = 1;
+	printf("%f:%d:", 123.43/2, 123.43 % i);
 	return (0);
 }
